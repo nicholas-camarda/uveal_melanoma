2494a21f8b054f03599a67f6c946cdd9adb47671
Nick Camarda <62025135+nicholas-camarda@users.noreply.github.com>
2025-09-21 16:38:11 -0400

    main execution latest commit, adding to gitignore for stability

--- scripts/main.R
+++ scripts/main.R
@@ -25,21 +25,21 @@ if (USE_LOGS) {
 
 # Uncomment the appropriate line below to run:
 # Run full analysis (all objectives, all datasets, merged tables)
-# main_execution()
+main_execution()
 
 # DEBUG: We are running specific objectives for all cohorts to debug each objective in isolation
 # Run specific objective for specific dataset and objective number
 # my_cohort <- "uveal_melanoma_full_cohort"
-my_cohort <- "uveal_melanoma_restricted_cohort"
+# my_cohort <- "uveal_melanoma_restricted_cohort"
 # # # my_cohort <- "uveal_melanoma_gksrs_only_cohort"
 
 # invisible(with_log_context(cohort = my_cohort, objective = "objective_0_data_processing", subobjective = NULL, expr = {
 #     run_specific_objective(my_cohort, 0)
 # }))
 
-invisible(with_log_context(cohort = my_cohort, objective = "objective_1_primary_outcomes", subobjective = NULL, expr = {
-    run_specific_objective(my_cohort, 1)
-}))
+# invisible(with_log_context(cohort = my_cohort, objective = "objective_1_primary_outcomes", subobjective = NULL, expr = {
+#     run_specific_objective(my_cohort, 1)
+# }))
 
 # invisible(with_log_context(cohort = my_cohort, objective = "objective_2_safety_toxicity", subobjective = NULL, expr = {
 #     run_specific_objective(my_cohort, 2)
