370407ff81d775c296442940ef9f71c504e74043
Nick Camarda <62025135+nicholas-camarda@users.noreply.github.com>
2025-09-21 14:19:47 -0400

    main.R main execution

--- scripts/main.R
+++ scripts/main.R
@@ -25,12 +25,12 @@ if (USE_LOGS) {
 
 # Uncomment the appropriate line below to run:
 # Run full analysis (all objectives, all datasets, merged tables)
-# main_execution()
+main_execution()
 
 # DEBUG: We are running specific objectives for all cohorts to debug each objective in isolation
 # Run specific objective for specific dataset and objective number
 # my_cohort <- "uveal_melanoma_full_cohort"
-my_cohort <- "uveal_melanoma_restricted_cohort"
+# my_cohort <- "uveal_melanoma_restricted_cohort"
 # # # my_cohort <- "uveal_melanoma_gksrs_only_cohort"
 
 # invisible(with_log_context(cohort = my_cohort, objective = "objective_0_data_processing", subobjective = NULL, expr = {
@@ -40,9 +40,9 @@ my_cohort <- "uveal_melanoma_restricted_cohort"
 #     run_specific_objective(my_cohort, 1)
 # }))
 
-invisible(with_log_context(cohort = my_cohort, objective = "objective_2_safety_toxicity", subobjective = NULL, expr = {
-    run_specific_objective(my_cohort, 2)
-}))
+# invisible(with_log_context(cohort = my_cohort, objective = "objective_2_safety_toxicity", subobjective = NULL, expr = {
+#     run_specific_objective(my_cohort, 2)
+# }))
 
 # invisible(with_log_context(cohort = my_cohort, objective = "objective_3_repeat_radiation", subobjective = NULL, expr = {
 #     run_specific_objective(my_cohort, 3)
