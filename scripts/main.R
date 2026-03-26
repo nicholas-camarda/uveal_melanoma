c083c13cb56bfdcfdcdcd0d248220ec07ecd8ae0
Nick Camarda <62025135+nicholas-camarda@users.noreply.github.com>
2026-03-25 20:22:54 -0400

    refactor(main.R): streamline execution flow by moving main_execution call and updating comments

--- scripts/main.R
+++ scripts/main.R
@@ -23,14 +23,17 @@ if (USE_LOGS) {
     setup_logging(log_path = NULL, level = "INFO", progress = interactive(), quiet_html = TRUE)
 }
 
+# All cohorts, all objectives
+main_execution()
+
 # Set these vectors to choose which cohorts and objectives to run.
 # Examples:
-# cohorts_to_run <- c("uveal_melanoma_full_cohort", "uveal_melanoma_restricted_cohort")
+# cohorts_to_run <- c("uveal_melanoma_full_cohort", "uveal_melanoma_restricted_cohort", "uveal_melanoma_gksrs_only_cohort") # Run all cohorts
 # objectives_to_run <- c(1, 4) # Run only objectives 1 and 4
-cohorts_to_run <- c("uveal_melanoma_full_cohort", "uveal_melanoma_gksrs_only_cohort")
-objectives_to_run <- c(4)
+# cohorts_to_run <- c("uveal_melanoma_full_cohort", "uveal_melanoma_gksrs_only_cohort")
+# objectives_to_run <- c(4)
 
-invisible(run_selected_objectives(cohorts_to_run, objectives_to_run))
+# invisible(run_selected_objectives(cohorts_to_run, objectives_to_run))
 
 # invisible(with_log_context(cohort = "all_cohorts", objective = "merged_tables", subobjective = NULL, expr = {
 #     merge_baseline_tables()
